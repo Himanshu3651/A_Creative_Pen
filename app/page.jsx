57388e9510091bf4c87cbc0df90b7aea5e82784f
Himanshu Singh <78560235+Himanshu3651@users.noreply.github.com>
2023-06-05 23:00:59 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Feed from "@components/Feed";
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+    A Creative Pen
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> Publish your passions, your way</span>
+    </h1>
+    <p className='desc text-center'>
+    . COME FOR WHAT YOU LOVE  .
+           <br/>
+          . STAY FOR WHAT YOU DISCOVER . 
+    </p>
+
+    <Feed />
+  </section>
+);
+
+export default Home;
